a9ac9ea630e95f058d0890fa119cd475440f31a0
Norbert Melzer <timmelzer@gmail.com>
2016-02-21 11:52:25 +0100

    Untabified and fixed Stack.push/2
    Stack.push/2 did reverse the underlying list twice just to prepend a
    single element.
    
    Using the cons-Operator does have the same result but beeing O(1)
    instead of O(n).

--- lib/data_structures/stack.ex
+++ lib/data_structures/stack.ex
@@ -1,132 +1,132 @@
 defmodule Exads.DataStructures.Stack do
 
-	@moduledoc """
-	An implementation of the Stack data structure with lists.
-	"""
+  @moduledoc """
+  An implementation of the Stack data structure with lists.
+  """
 
-	@doc """
-	Returns a new empty stack.
-	"""
-	@spec new() :: []
+  @doc """
+  Returns a new empty stack.
+  """
+  @spec new() :: []
 
-	def new, do: []
+  def new, do: []
 
-	@doc """
-	Return the stack with the given element pushed into it.
-	"""
-	@spec push(list(any()), any()) :: list(any())
+  @doc """
+  Return the stack with the given element pushed into it.
+  """
+  @spec push(list(any()), any()) :: list(any())
 
-	def push(stack, elem) do 
-		(Enum.reverse(stack) ++ [elem]) |> Enum.reverse
-	end
+  def push(stack, elem) do
+    [elem|stack]
+  end
 
-	@doc """
-	Pops the top element from the stack returning a tuple with the format
-	{element, new_list}
-	"""
-	@spec pop(list(any())) :: tuple()
+  @doc """
+  Pops the top element from the stack returning a tuple with the format
+  {element, new_list}
+  """
+  @spec pop(list(any())) :: tuple()
 
-	def pop([]), do: nil
-	def pop([head | tail] = _stack) do
-		{head, tail}
-	end
+  def pop([]), do: nil
+  def pop([head | tail] = _stack) do
+    {head, tail}
+  end
 
-	@doc """
-	Deletes the top element from the stack.
-	"""
-	@spec delete(list(any())) :: list(any())
+  @doc """
+  Deletes the top element from the stack.
+  """
+  @spec delete(list(any())) :: list(any())
 
-	def delete([]), do: nil
-	def delete(stack) do 
-		{_, result} = pop(stack)
-		result
-	end
+  def delete([]), do: nil
+  def delete(stack) do 
+    {_, result} = pop(stack)
+    result
+  end
 
-	@doc """
-	Returns true if the stack is empty or false otherwise.
-	"""
-	@spec empty?([]) :: boolean
+  @doc """
+  Returns true if the stack is empty or false otherwise.
+  """
+  @spec empty?([]) :: boolean
 
-	def empty?(stack) do 
-		Enum.empty? stack
-	end
+  def empty?(stack) do 
+    Enum.empty? stack
+  end
 
-	@doc """
-	Returns the top element from the stack without removing it. If the stack
-	is empty returns nil.
-	"""
-	@spec top(list(any())) :: any() | nil
+  @doc """
+  Returns the top element from the stack without removing it. If the stack
+  is empty returns nil.
+  """
+  @spec top(list(any())) :: any() | nil
 
-	def top([]), do: nil
-	def top(stack) do 
-		stack |> List.first 
-	end
+  def top([]), do: nil
+  def top(stack) do 
+    stack |> List.first 
+  end
 
-	@doc """
-	Returns the maximum element in the stack using Elixir's built-in hierarchy.
-	"""
-	@spec max(list(any())) :: any() | nil
+  @doc """
+  Returns the maximum element in the stack using Elixir's built-in hierarchy.
+  """
+  @spec max(list(any())) :: any() | nil
 
-	def max(stack) do
-		stack |> Enum.sort |> List.last
-	end
+  def max(stack) do
+    stack |> Enum.sort |> List.last
+  end
 
-	@doc """
-	Given a stack and an element, returns true if the element is a member
-	of the stack or false otherwise.
-	"""
-	@spec member?(list(any()), any()) :: boolean
+  @doc """
+  Given a stack and an element, returns true if the element is a member
+  of the stack or false otherwise.
+  """
+  @spec member?(list(any()), any()) :: boolean
 
-	def member?(stack, elem) do 
-		Enum.member? stack, elem
-	end
+  def member?(stack, elem) do 
+    Enum.member? stack, elem
+  end
 
-	@doc """
-	Returns the position in the stack of a given element. Returns -1 if the
-	element is not present. If the element appears more than once, then the 
-	first occurrence is considered.
-	"""
-	@spec position(list(any()), any()) :: integer
+  @doc """
+  Returns the position in the stack of a given element. Returns -1 if the
+  element is not present. If the element appears more than once, then the 
+  first occurrence is considered.
+  """
+  @spec position(list(any()), any()) :: integer
 
-	def position(stack, elem) do 
-		get_pos stack, elem, 1
-	end
+  def position(stack, elem) do 
+    get_pos stack, elem, 1
+  end
 
-	defp get_pos([], _, _), do: -1
-	defp get_pos([head | tail], elem, count) do 
-		if head == elem do 
-			count
-		else
-			get_pos tail, elem, count + 1
-		end
-	end
+  defp get_pos([], _, _), do: -1
+  defp get_pos([head | tail], elem, count) do 
+    if head == elem do 
+    count
+    else
+      get_pos tail, elem, count + 1
+    end
+  end
 
-	@doc """
-	Given a stack and an element returns true if element appears more than
-	once in the stack or false otherwise.
-	"""
-	@spec more_than_once(list(any()), any()) :: boolean
+  @doc """
+  Given a stack and an element returns true if element appears more than
+  once in the stack or false otherwise.
+  """
+  @spec more_than_once(list(any()), any()) :: boolean
 
-	def more_than_once(stack, elem), do: mto stack, elem
+  def more_than_once(stack, elem), do: mto stack, elem
 
-	defp mto([], _), do: false
-	defp mto([head | tail], elem) do
-		if head == elem do 
-			if position(tail, elem) >= 1 do 
-				true
-			else
-				false
-			end
-		else
-			mto tail, elem
-		end
-	end
+  defp mto([], _), do: false
+  defp mto([head | tail], elem) do
+    if head == elem do 
+      if position(tail, elem) >= 1 do 
+      true
+      else
+        false
+      end
+    else
+      mto tail, elem
+    end
+  end
 
-	@doc """
-	Returns the size of the stack.
-	"""
-	@spec size(list(any())) :: pos_integer()
+  @doc """
+  Returns the size of the stack.
+  """
+  @spec size(list(any())) :: pos_integer()
 
-	def size(stack), do: length(stack)
+  def size(stack), do: length(stack)
 
 end
