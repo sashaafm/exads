d60f27fc377c36bb74ffe77610a6d004f88d419f
Sasha Fonseca <sashaafm@gmail.com>
2016-02-22 22:20:38 +0000

    Implemented the binary search algo and a small test suite.

---
+++ test/binary_search_test.exs
@@ -0,0 +1,44 @@
+defmodule BinarySearchTest do
+	use ExUnit.Case, async: true
+  alias Exads.Algorithms.BinarySearch, as: BS
+  doctest Exads
+
+  test "empty list" do 
+  	assert BS.binary_search([], 4) == nil
+  end
+
+  test "one elem list" do 
+  	assert BS.binary_search([4], 4) == 0
+  end
+
+  test "two elem list" do 
+  	assert BS.binary_search([1, 2], 2) == 1
+  	assert BS.binary_search([1, 2], 1) == 0
+  end
+
+  test "short list" do 
+  	assert BS.binary_search([4, 5, 8, 9, 18, 29, 57, 79], 29) == 5
+  	assert BS.binary_search([4, 5, 8, 9, 18, 29, 57, 79], 79) == 7
+  	assert BS.binary_search([4, 5, 8, 9, 18, 29, 57, 79], 4)  == 0
+  	assert BS.binary_search([4, 5, 8, 9, 18, 29, 57, 79], 9)  == 3
+  end
+
+  test "medium list" do 
+  	assert BS.binary_search((1..10_000) |> Enum.into([]), 1) 			== 0
+  	assert BS.binary_search((1..10_000) |> Enum.into([]), 10_000) == 9999
+  	assert BS.binary_search((1..10_000) |> Enum.into([]), 5000) 	== 4999
+  end
+
+  test "long list" do
+  	spawn fn -> 
+  		assert BS.binary_search((1..1_000_000) |> Enum.into([]), 1) 				== 0
+  	end
+  	spawn fn ->
+  		assert BS.binary_search((1..1_000_000) |> Enum.into([]), 10_000) 		== 9999
+  	end
+  	spawn fn ->
+  		assert BS.binary_search((1..1_000_000) |> Enum.into([]), 1_000_000) == 999999
+  	end
+  end
+	
+end
