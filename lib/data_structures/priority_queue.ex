9d5aa8791b63fc4b50a55ad61d709938f17ada8d
James Wheaton <jwheaton@kpmg.com>
2016-02-26 11:09:06 -0700

    Fix whitespace issues - convert tabs to 2-space indentions

--- lib/data_structures/priority_queue.ex
+++ lib/data_structures/priority_queue.ex
@@ -1,161 +1,161 @@
 defmodule Exads.DataStructures.PriorityQueue do
 
-	@moduledoc """
-	An implementation of the Priority Queue data structure with list and 
-	tuples. The priority is recommended to be set by integers, but may be set
-	using any type (evaluation will be done using Erlang's order system).
-	"""
+  @moduledoc """
+  An implementation of the Priority Queue data structure with list and
+  tuples. The priority is recommended to be set by integers, but may be set
+  using any type (evaluation will be done using Erlang's order system).
+  """
 
-	@doc """
-	Returns a new empty queue.
-	"""
-	@spec new() :: []
+  @doc """
+  Returns a new empty queue.
+  """
+  @spec new() :: []
 
-	def new(), do: []
+  def new(), do: []
 
-	@doc """
-	Inserts the element into the queue by FIFO policy.
-	"""
-	@spec insert_with_priority(list(tuple()), tuple()) :: list(tuple())
+  @doc """
+  Inserts the element into the queue by FIFO policy.
+  """
+  @spec insert_with_priority(list(tuple()), tuple()) :: list(tuple())
 
-	def insert_with_priority(queue, {_item, _prio} = elem) do 
-		queue ++ [elem]
-	end
+  def insert_with_priority(queue, {_item, _prio} = elem) do
+    queue ++ [elem]
+  end
 
-	@doc """
-	Returns the element that is next up on the queue by priority. If two or more
-	elements have the same priority, then the FIFO policy is used.
-	"""
-	@spec get_frontmost_element(list(tuple())) :: {tuple(), list(tuple())}
+  @doc """
+  Returns the element that is next up on the queue by priority. If two or more
+  elements have the same priority, then the FIFO policy is used.
+  """
+  @spec get_frontmost_element(list(tuple())) :: {tuple(), list(tuple())}
 
-	def get_frontmost_element(queue) do 
-		gfe queue
-	end
+  def get_frontmost_element(queue) do
+    gfe queue
+  end
 
-	@doc """
-	Removes the element from the queue without returning it.
-	"""
-	@spec delete(list(tuple())) :: list(tuple())
+  @doc """
+  Removes the element from the queue without returning it.
+  """
+  @spec delete(list(tuple())) :: list(tuple())
 
-	def delete([]), do: []
-	def delete([_head | tail]), do: tail
+  def delete([]), do: []
+  def delete([_head | tail]), do: tail
 
-	@doc """
-	Returns true if the list is empty or false otherwise.
-	"""
-	@spec empty?(list(tuple())) :: boolean()
+  @doc """
+  Returns true if the list is empty or false otherwise.
+  """
+  @spec empty?(list(tuple())) :: boolean()
 
-	def empty?(queue), do: Enum.empty? queue
+  def empty?(queue), do: Enum.empty? queue
 
-	@doc """
-	Returns the element that is next up without removing it.
-	"""
-	@spec front(list(tuple())) :: tuple() | nil
+  @doc """
+  Returns the element that is next up without removing it.
+  """
+  @spec front(list(tuple())) :: tuple() | nil
 
-	def front([]), do: nil
-	def front(queue) do 
-		{result, _} = gfe queue
-		result
-	end
+  def front([]), do: nil
+  def front(queue) do
+    {result, _} = gfe queue
+    result
+  end
 
-	defp gfe([]), do: nil
-	defp gfe(queue) do 
-		{_, front_elem} = queue
-											|> Enum.group_by(fn {_, prio} -> prio end)
-											|> Map.to_list
-											|> List.last
+  defp gfe([]), do: nil
+  defp gfe(queue) do
+    {_, front_elem} = queue
+                      |> Enum.group_by(fn {_, prio} -> prio end)
+                      |> Map.to_list
+                      |> List.last
 
-		{List.last(front_elem), queue -- [List.last(front_elem)]}
-	end
+    {List.last(front_elem), queue -- [List.last(front_elem)]}
+  end
 
-	@doc """
-	Returns the if the given 'elem' is a member of the given 'queue' or false
-	otherwise.
-	"""
-	@spec member?(list(tuple()), tuple()) :: boolean()
-	
-	def member?(queue, elem), do: Enum.member? queue, elem
+  @doc """
+  Returns the if the given 'elem' is a member of the given 'queue' or false
+  otherwise.
+  """
+  @spec member?(list(tuple()), tuple()) :: boolean()
 
-	@doc """
-	Returns the position by FIFO policy of the given 'elem' in the given 'queue'.
-	"""
-	@spec position_by_order(list(tuple()), tuple()) :: integer()
+  def member?(queue, elem), do: Enum.member? queue, elem
 
-	def position_by_order(queue, elem) do 
-		get_pos queue, elem, 1
-	end
+  @doc """
+  Returns the position by FIFO policy of the given 'elem' in the given 'queue'.
+  """
+  @spec position_by_order(list(tuple()), tuple()) :: integer()
 
-	defp get_pos([], _, _), do: -1
-	defp get_pos([head | tail], elem, count) do 
-		if head == elem do 
-			count
-		else
-			get_pos tail, elem, count + 1
-		end
-	end	
+  def position_by_order(queue, elem) do
+    get_pos queue, elem, 1
+  end
 
-	@doc """
-	Returns the position by priority policy of the given 'elem' in the given
-	'queue'. If there are duplicate elements, then the position is determined
-	by FIFO policy.
-	"""
-	@spec position_by_priority(list(tuple()), tuple()) :: integer()
+  defp get_pos([], _, _), do: -1
+  defp get_pos([head | tail], elem, count) do
+    if head == elem do
+      count
+    else
+      get_pos tail, elem, count + 1
+    end
+  end
 
-	def position_by_priority([]), do: -1
-	def position_by_priority(queue, elem) do 
-		find_elem_by_priority queue, elem, 1
-	end
+  @doc """
+  Returns the position by priority policy of the given 'elem' in the given
+  'queue'. If there are duplicate elements, then the position is determined
+  by FIFO policy.
+  """
+  @spec position_by_priority(list(tuple()), tuple()) :: integer()
 
-	defp find_elem_by_priority([], _, _), do: -1
-	defp find_elem_by_priority([_head | tail] = queue, elem, count) do 
-		if elem == front(queue) do 
-			count
-		else
-			find_elem_by_priority tail, elem, count + 1
-		end
-	end
+  def position_by_priority([]), do: -1
+  def position_by_priority(queue, elem) do
+    find_elem_by_priority queue, elem, 1
+  end
 
-	@doc """
-	Returns true if the given 'elem' appears in the 'queue' more than once or
-	false otherwise.
-	"""
-	@spec more_than_once(list(tuple()), tuple()) :: boolean()
+  defp find_elem_by_priority([], _, _), do: -1
+  defp find_elem_by_priority([_head | tail] = queue, elem, count) do
+    if elem == front(queue) do
+      count
+    else
+      find_elem_by_priority tail, elem, count + 1
+    end
+  end
 
-	def more_than_once(queue, elem), do: mto queue, elem
+  @doc """
+  Returns true if the given 'elem' appears in the 'queue' more than once or
+  false otherwise.
+  """
+  @spec more_than_once(list(tuple()), tuple()) :: boolean()
 
-	defp mto([], _), do: false
-	defp mto([head | tail], elem) do
-		if head == elem do 
-			if position_by_order(tail, elem) >= 1 do 
-				true
-			else
-				false
-			end
-		else
-			mto tail, elem
-		end
-	end
+  def more_than_once(queue, elem), do: mto queue, elem
 
-	@doc """
-	Returns the size of the 'queue'.
-	"""
-	@spec size(list(tuple())) :: non_neg_integer()
+  defp mto([], _), do: false
+  defp mto([head | tail], elem) do
+    if head == elem do
+      if position_by_order(tail, elem) >= 1 do
+        true
+      else
+        false
+      end
+    else
+      mto tail, elem
+    end
+  end
 
-	def size(queue), do: length queue
+  @doc """
+  Returns the size of the 'queue'.
+  """
+  @spec size(list(tuple())) :: non_neg_integer()
 
-	@doc """
-	Increases the priority of a given element in the queue, if the new priority
-	is at least as large as the old one.
-	"""
-	@spec increase_element_priority(list(tuple()), tuple(), any()) :: list(tuple()) | []
+  def size(queue), do: length queue
 
-	def increase_element_priority([], _, _), do: []
-	def increase_element_priority(queue, {item, prio} = elem, new_prio) 
-																										when new_prio >= prio do
-		pos 			 = position_by_order queue, elem 
-		first_half = (queue |> Enum.take(pos)) -- [elem]
-		sec_half 	 = queue 	|> Enum.slice(pos, length(queue))
+  @doc """
+  Increases the priority of a given element in the queue, if the new priority
+  is at least as large as the old one.
+  """
+  @spec increase_element_priority(list(tuple()), tuple(), any()) :: list(tuple()) | []
 
-		first_half ++ [{item, new_prio}] ++ sec_half
-	end
+  def increase_element_priority([], _, _), do: []
+  def increase_element_priority(queue, {item, prio} = elem, new_prio)
+    when new_prio >= prio do
+      pos        = position_by_order queue, elem
+      first_half = (queue |> Enum.take(pos)) -- [elem]
+      sec_half   = queue  |> Enum.slice(pos, length(queue))
+
+      first_half ++ [{item, new_prio}] ++ sec_half
+  end
 end
--- lib/data_structures/queue.ex
+++ lib/data_structures/queue.ex
@@ -1,119 +1,118 @@
 defmodule Exads.DataStructures.Queue do
 
-	@moduledoc """
-	An implmentation of the Queue data structure with lists.
-	"""
+  @moduledoc """
+  An implmentation of the Queue data structure with lists.
+  """
 
-	@doc """
-	Returns a new empty queue.
-	"""
-	@spec new() :: []
+  @doc """
+  Returns a new empty queue.
+  """
+  @spec new() :: []
 
-	def new, do: []
+  def new, do: []
 
-	@doc """
-	Inserts a new element into the queue and returns the resulting queue.
-	"""
-	@spec enqueue(list(any()), any()) :: list(any())
+  @doc """
+  Inserts a new element into the queue and returns the resulting queue.
+  """
+  @spec enqueue(list(any()), any()) :: list(any())
 
-	def enqueue(queue, elem) do 
-		queue ++ [elem]
-	end
+  def enqueue(queue, elem) do
+    queue ++ [elem]
+  end
 
-	@doc """
-	Removes the first element from the queue returning a tuple {element, result}.
-	"""
-	@spec dequeue(list(any())) :: tuple()
+  @doc """
+  Removes the first element from the queue returning a tuple {element, result}.
+  """
+  @spec dequeue(list(any())) :: tuple()
 
-	def dequeue([]), do: {nil, []}
-	def dequeue([head | tail]) do 
-		{head, tail}
-	end
+  def dequeue([]), do: {nil, []}
+  def dequeue([head | tail]) do
+    {head, tail}
+  end
 
-	@doc """
-	Deletes the first element from the queue returning the resulting queue.
-	"""
-	@spec delete(list(any())) :: list(any())
+  @doc """
+  Deletes the first element from the queue returning the resulting queue.
+  """
+  @spec delete(list(any())) :: list(any())
 
-	def delete([]), do: []
-	def delete([_head | tail]), do: tail
+  def delete([]), do: []
+  def delete([_head | tail]), do: tail
 
-	@doc """
-	Returns true if the queue is empty or false otherwise.
-	"""
-	@spec empty?(list(any())) :: boolean
-	
-	def empty?(queue), do: Enum.empty? queue
+  @doc """
+  Returns true if the queue is empty or false otherwise.
+  """
+  @spec empty?(list(any())) :: boolean
 
-	@doc """
-	Returns the first element in the queue.
-	"""
-	@spec first(list(any())) :: any() | nil
+  def empty?(queue), do: Enum.empty? queue
 
-	def first([]), do: nil
-	def first([head | _tail]), do: head
+  @doc """
+  Returns the first element in the queue.
+  """
+  @spec first(list(any())) :: any() | nil
 
-	@doc """
-	Returns the maximum element in the queue using Elixir's built-in hierarchy.
-	"""
-	@spec max(list(any())) :: any() | nil
+  def first([]), do: nil
+  def first([head | _tail]), do: head
 
-	def max(queue), do: queue |> Enum.sort |> List.last
+  @doc """
+  Returns the maximum element in the queue using Elixir's built-in hierarchy.
+  """
+  @spec max(list(any())) :: any() | nil
 
-	@doc """
-	Returns true if the elem is a member of the queue or false otherwise.
-	"""
-	@spec member?(list(any()), any()) :: boolean()
+  def max(queue), do: queue |> Enum.sort |> List.last
 
-	def member?(queue, elem), do: Enum.member? queue, elem
+  @doc """
+  Returns true if the elem is a member of the queue or false otherwise.
+  """
+  @spec member?(list(any()), any()) :: boolean()
 
-	@doc """
-	Returns the position in the queue of a given element. Returns -1 if the
-	element is not present. If the element appears more than once, then the 
-	first occurrence is considered.
-	"""
-	@spec position(list(any()), any()) :: integer
+  def member?(queue, elem), do: Enum.member? queue, elem
 
-	def position(queue, elem) do 
-		get_pos queue, elem, 1
-	end
+  @doc """
+  Returns the position in the queue of a given element. Returns -1 if the
+  element is not present. If the element appears more than once, then the
+  first occurrence is considered.
+  """
+  @spec position(list(any()), any()) :: integer
 
-	defp get_pos([], _, _), do: -1
-	defp get_pos([head | tail], elem, count) do 
-		if head == elem do 
-			count
-		else
-			get_pos tail, elem, count + 1
-		end
-	end
+  def position(queue, elem) do
+    get_pos queue, elem, 1
+  end
 
-	@doc """
-	Given a queue and an element returns true if element appears more than
-	once in the queue or false otherwise.
-	"""
-	@spec more_than_once(list(any()), any()) :: boolean
+  defp get_pos([], _, _), do: -1
+  defp get_pos([head | tail], elem, count) do
+    if head == elem do
+      count
+    else
+      get_pos tail, elem, count + 1
+    end
+  end
 
-	def more_than_once(queue, elem), do: mto queue, elem
+  @doc """
+  Given a queue and an element returns true if element appears more than
+  once in the queue or false otherwise.
+  """
+  @spec more_than_once(list(any()), any()) :: boolean
 
-	defp mto([], _), do: false
-	defp mto([head | tail], elem) do
-		if head == elem do 
-			if position(tail, elem) >= 1 do 
-				true
-			else
-				false
-			end
-		else
-			mto tail, elem
-		end
-	end
+  def more_than_once(queue, elem), do: mto queue, elem
 
-	@doc """
-	Returns the size of the queue.
-	"""
-	@spec size(list(any())) :: pos_integer()
+  defp mto([], _), do: false
+  defp mto([head | tail], elem) do
+    if head == elem do
+      if position(tail, elem) >= 1 do
+        true
+      else
+        false
+      end
+    else
+      mto tail, elem
+    end
+  end
 
-	def size(queue), do: length(queue)
+  @doc """
+  Returns the size of the queue.
+  """
+  @spec size(list(any())) :: pos_integer()
 
+  def size(queue), do: length(queue)
 
 end
