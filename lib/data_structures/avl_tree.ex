ccc3c3dc3cdeaecd9b0782f1b9c3c10b4454b07a
Mathias Remshardt <mathias.remshardt@logicline.de>
2017-01-05 16:30:39 +0100

    Adds first version of avl insert * Does not perform rotations

--- lib/data_structures/avl_tree.ex
+++ lib/data_structures/avl_tree.ex
@@ -2,6 +2,13 @@ defmodule Exads.DataStructures.AVLTree do
 
   alias Exads.DataStructures.BinarySearchTree, as: BST
 
+  defmodule Augmentation do
+
+    @type augmentation :: %__MODULE__{height: integer, bf: integer}
+    defstruct height: 0, bf: 0
+
+  end
+
 
   @moduledoc """
   An implementation of the AVL Tree abstract data structure
@@ -12,7 +19,23 @@ defmodule Exads.DataStructures.AVLTree do
   """
   @spec new(any) :: BST.Node.bst_node
 
-  def new(value), do: BST.new(value)
+  def new(value), do: BST.new(value, &augment/1)
+
+  def insert(tree, node_value) do
+    BST.insert(tree, node_value, &augment/1)
+  end
 
 
+  @spec augment(BST.Node.bst_node | :leaf) :: BST.Node.bst_node
+  defp augment(:leaf), do: :leaf
+  defp augment(%BST.Node{left: :leaf, right: :leaf} = node), do: %{node | augmentation: %Augmentation{}}
+  defp augment(%BST.Node{left: left, right: :leaf} = node) do
+    %{node | augmentation: %Augmentation{height: left.augmentation.height + 1, bf: left.augmentation.height + 1}}
+  end
+  defp augment(%BST.Node{left: :leaf, right: right} = node) do
+    %{node | augmentation: %Augmentation{height: right.augmentation.height + 1, bf: -1 * right.augmentation.height - 1}}
+  end
+  defp augment(%BST.Node{left: left, right: right} = node), do:
+    %{node | augmentation: %Augmentation{height:  max(left.augmentation.height, right.augmentation.height) + 1,
+                                         bf:      left.augmentation.height - right.augmentation.height}}
 end
--- test/avl_tree_test.exs
+++ test/avl_tree_test.exs
@@ -4,12 +4,52 @@ defmodule Exads.DataStructures.AVLTreeTest do
   alias Exads.DataStructures.BinarySearchTree, as: BST
   alias Exads.DataStructures.AVLTree, as: AVL
 
+  setup do
+    tree = AVL.new(6) |> AVL.insert(1) |> AVL.insert(12)
+    {:ok, %{tree: tree}}
+  end
+
   test "new" do
     assert AVL.new(0) ==
-      %BST.Node{left:   :leaf,
-                right:  :leaf,
-                value:  0}
+      %BST.Node{left:         :leaf,
+                right:        :leaf,
+                value:        0,
+                augmentation: %AVL.Augmentation{height: 0, bf: 0}}
   end
+
+  test "right-heavy insert with no rotation required", %{tree: tree} do
+    assert tree |> AVL.insert(14) ==
+      %BST.Node{left:         %BST.Node{left:         :leaf,
+                                        right:        :leaf,
+                                        value:        1,
+                                        augmentation: %AVL.Augmentation{height: 0, bf: 0}},
+                right:        %BST.Node{left:         :leaf,
+                                        right:        %BST.Node{left:         :leaf,
+                                                                right:        :leaf,
+                                                                value:        14,
+                                                                augmentation: %AVL.Augmentation{height: 0, bf: 0}},
+                                        value:        12,
+                                        augmentation: %AVL.Augmentation{height: 1, bf: -1}},
+                value:  6,
+                augmentation: %AVL.Augmentation{height: 2, bf: -1}}
+  end
+
+  test "left-heavy insert with no rotation required", %{tree: tree} do
+      assert tree |> AVL.insert(2) ==
+        %BST.Node{left:         %BST.Node{left:         :leaf,
+                                          right:        %BST.Node{left:         :leaf,
+                                                                  right:        :leaf,
+                                                                  value:        2,
+                                                                  augmentation: %AVL.Augmentation{height: 0, bf: 0}},
+                                          value:        1,
+                                          augmentation: %AVL.Augmentation{height: 1, bf: -1}},
+                  right:        %BST.Node{left:         :leaf,
+                                          right:        :leaf,
+                                          value:        12,
+                                          augmentation: %AVL.Augmentation{height: 0, bf: 0}},
+                  value:  6,
+                  augmentation: %AVL.Augmentation{height: 2, bf: 1}}
+    end
 
 
 
